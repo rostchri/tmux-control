9792e5eda7d4e89d5873383afe605ce295a5a8f5
Maximilian Middeke <mam@baltic-online.de>
2017-08-07 23:55:41 +0200

    added stuff to fill

--- tmuxMenu.py
+++ tmuxMenu.py
@@ -4,24 +4,58 @@ import libtmux
 import os
 import sys
 
+configDir = "configs"
 server = libtmux.Server()
+
+
+class Config:
+
+    def __init__(self, name):
+        self.name = name
+        self.file = os.path.join(configDir, self.name + ".txt")
+        if self.file in os.listdir(configDir):
+            with open(self.file, "r") as f:
+                self.rawLines = f.readlines()
+                self.data = self.loadData()
+        else:
+            self.data = self.makeConfig()
+    
+    def create(self):
+        pass
+    
+    def makeConfig(self):
+        pass
+        
+    def loadData(self):
+        self.machineNames, self.machineInfos = [], []
+        for line in self.rawLines:
+            self.machineNames, self.machineInfos .append(line.split(","))
+
+    def saveConfig(self):
+        self.savingData = self.data.replace("a", "b")
+        with open(self.file, "w") as f:
+            f.write(self.savingData)
+
 
 class Menu:
 
     def __init__(self, name, options, prompt):
-        
         self.menuDict = {}
         self.name = name
         self.options = options
-        for option in self.options:
-            self.menuDict[options.index(option)] = option
-            self.menuDict[options.index(option)][0] = "{0}".format(self.menuDict[options.index(option)][0])
         self.prompt = prompt
-        i = 0
-        self.text = ""
-        for option in self.menuDict:
-            self.text += "{0}: {1}\n".format(i+1, self.menuDict[option][0])
-            i+=1
+        self.text = self.buildMenu()
+
+    def buildMenu(self):
+        for option in self.options:
+            self.menuDict[self.options.index(option)] = option
+            self.menuDict[self.options.index(option)][0] = "{0}".format(self.menuDict[self.options.index(option)][0])
+            i = 0
+            self.text = ""
+            for option in self.menuDict:
+                self.text += "{0}: {1}\n".format(i+1, self.menuDict[option][0])
+                i+=1
+        return(self.text)
 
     def launch(self):
         print(self.text)
@@ -37,7 +71,7 @@ class Menu:
 def app():
     if __name__ == "__main__":
         prompt = buildPrompt()
-        mainMenu(prompt)
+        main(prompt)
 
 
 def buildPrompt():
@@ -57,18 +91,23 @@ def functionB():
     print("You called function 2")
 
 
-def mainMenu(prompt):
-    options = [
-        ["first action", functionA],
-        ["second action", functionB],
-        ["quit", quit],
-    ]
-    thisMenu = Menu("main menu", options, prompt)
-    thisMenu.launch()
+def getConfigs():
+    pass
 
 
 def handle(func):
     func()
+
+
+def main(prompt):
+
+    startMenu = [
+        ["first action", functionA],
+        ["second action", functionB],
+        ["quit", quit],
+    ]
+    thisMenu = Menu("start menu", startMenu, prompt)
+    thisMenu.launch()
 
 
 app()
