2305adced3b59ddc22b2b6ec8aa03de1c66c8d09
Maximilian Middeke <mam@baltic-online.de>
2017-08-09 19:21:57 +0200

    renamed files

---
+++ frontend.py
@@ -0,0 +1,20 @@
+import curses
+
+standardScreen = curses.initscr()
+
+
+class Layout:
+    def __init__(self, title):
+        pass
+
+
+def build():
+    pass
+
+
+def show():
+    pass
+
+
+def update():
+    pass
