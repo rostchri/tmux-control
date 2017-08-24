aa7802997a85e5af57713415453151865cf280c4
Maximilian Middeke <mam@baltic-online.de>
2017-08-24 15:01:37 +0200

    code layout (alphabetized functions etc)

--- frontend.py
+++ frontend.py
@@ -3,6 +3,24 @@
 import curses
 
 
+appName = 'tmuxControl'
+appVersion = 'v0.1'
+
+release = '{0} {1}'.format(appName, appVersion)
+gitLink = 'https://github.com/2357mam/tmuxControl'
+footer = 'source: {0}'.format(gitLink)
+
+# assigning colors
+red = curses.COLOR_RED
+green = curses.COLOR_GREEN
+blue = curses.COLOR_BLUE
+yellow = curses.COLOR_YELLOW
+black = curses.COLOR_BLACK        
+magenta = curses.COLOR_MAGENTA
+cyan = curses.COLOR_CYAN
+white = curses.COLOR_WHITE
+
+
 # iterates through the menu-contents dict:
 #   determining height/ width of the desired box
 #   creating a dict that holds line-numbers and corresponding values
@@ -12,16 +30,18 @@ def getBoxProperties():
     boxHeight = 0
     boxWidth = 0
     line = 3
-    
+
     category = 'header'
     propertyDict[category] = []
+    propertyDict[category].append([line, ''])
+    line, boxHeight = map(incrementVars,[line, boxHeight])
     for row in contentDict[category]:
         var = row
         propertyDict[category].append([line, var])
         line, boxHeight = map(incrementVars,[line, boxHeight])
         if len(var) > boxWidth:
             boxWidth = len(var)
-    propertyDict[category].append([line, '---'])
+    propertyDict[category].append([line, ''])
     line, boxHeight = map(incrementVars,[line, boxHeight])
 
     category = 'content'
@@ -37,7 +57,7 @@ def getBoxProperties():
                 line, boxHeight = map(incrementVars,[line, boxHeight])
         if len(var) > boxWidth:
             boxWidth = len(var)
-    propertyDict[category].append([line, '---'])
+    propertyDict[category].append([line, ''])
     line, boxHeight = map(incrementVars,[line, boxHeight])
 
     category = 'footer'
@@ -48,34 +68,15 @@ def getBoxProperties():
         line, boxHeight = map(incrementVars,[line, boxHeight])
         if len(var) > boxWidth:
             boxWidth = len(var)
+    propertyDict[category].append([line, ''])
+    line, boxHeight = map(incrementVars,[line, boxHeight])
         
     boxWidth += 4
     return propertyDict, boxHeight, boxWidth
 
 
-# builds the actual menu:
-#   1. gets the properties by calling getBoxProperties
-#   2. adds the strings to their respective places and refreshes the window
-def makeBox():
-    propertyDict, boxHeight, boxWidth = getBoxProperties()
-    win = curses.newwin(boxHeight + 2, boxWidth, 5, 35)
-    win.bkgd(curses.color_pair(2))
-
-    for line in propertyDict['header']:
-        win.addstr(line[0] -2, 2, line[1])
-
-    for line in propertyDict['content']:
-        for entity in line:
-            if not type(entity) == list:
-                win.addstr(line[0] -2, 2, line[1])
-            else:
-                win.addstr(entity[0] -2, 2, entity[1])
-
-    for line in propertyDict['footer']:
-        win.addstr(line[0] -2, 2, line[1]) 
-
-    win.box()
-    win.refresh()
+def incrementVars(x):
+    return x+1
 
 
 # default initialization, receives two pairs of colours (defined at the top of this document)
@@ -92,6 +93,27 @@ def initCurses(c1, c2, c3, c4):
     return stdscr
 
 
+def initSession():
+    # dict holding the menu-layout-categories (header content footer)
+    contentDict = {
+        'header' : [release],
+        'content' : [
+            'foo',
+            'bar',
+            'baz'
+        ],
+        'footer' : [footer]
+    }
+
+    # assigning handy vars to neutral parameters
+    c1 = green
+    c2 = green
+    c3 = black
+    c4 = green
+
+    launch(contentDict, c1, c2, c3, c4)
+
+
 # terminates the box
 def killBox(stdscr):
     curses.nocbreak()
@@ -100,6 +122,7 @@ def killBox(stdscr):
     curses.endwin()
 
 
+# c1= inactive, c2=windowBg, c3=boxText, c4=boxBg
 def launch(contentDict, c1, c2, c3, c4):
     stdscr = initCurses(c1, c2, c3, c4)
     makeBox()
@@ -108,33 +131,26 @@ def launch(contentDict, c1, c2, c3, c4):
     killBox(stdscr)
 
 
-def incrementVars(x):
-    return x+1
+# builds the actual menu:
+#   1. gets the properties by calling getBoxProperties
+#   2. adds the strings to their respective places and refreshes the window
+def makeBox():
+    propertyDict, boxHeight, boxWidth = getBoxProperties()
+    win = curses.newwin(boxHeight + 2, boxWidth, 5, 35)
+    win.bkgd(curses.color_pair(2))
+    for line in propertyDict['header']:
+        win.addstr(line[0] -2, 2, line[1])
+    for line in propertyDict['content']:
+        for entity in line:
+            if not type(entity) == list:
+                win.addstr(line[0] -2, 2, line[1])
+            else:
+                win.addstr(entity[0] -2, 2, entity[1])
+    for line in propertyDict['footer']:
+        win.addstr(line[0] -2, 2, line[1]) 
+    win.box()
+    win.refresh()
 
 
 if __name__ == '__main__':
-
-    # dict holding the menu-layout-categories (header content footer)
-    contentDict = {
-        'header' : ['tmux Control 0.1'],
-        'content' : [
-            'foo',
-            'bar',
-            'baz'
-        ],
-        'footer' : ['tcFooter']
-    }
-
-    # assigning some colors to handy vars
-    green = curses.COLOR_GREEN
-    blue = curses.COLOR_BLUE
-    yellow = curses.COLOR_YELLOW
-    black = curses.COLOR_BLACK
-
-    # assigning handy vars to neutral parameters
-    c1 = green
-    c2 = blue
-    c3 = yellow
-    c4 = black
-
-    launch(contentDict, c1, c2, c3, c4)
+    initSession()
